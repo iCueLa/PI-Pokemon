6fd3cf3fa2a4ddb77301886a761760ee783c6267
iCueLa <mambito1998@gmail.com>
2021-07-03 15:24:41 -0300

    listo

---
+++ api/src/models/Type.js
@@ -0,0 +1,19 @@
+const {DataTypes} = require("sequelize")
+
+module.exports = (sequelize) => {
+
+
+sequelize.define('type',{
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name:{
+      type: DataTypes.STRING
+    }
+  })
+
+
+};
